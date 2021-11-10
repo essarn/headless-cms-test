68b211321cff8613456f50787e9ac96c501dc30c
Isak <16906089+essarn@users.noreply.github.com>
2021-11-10 12:34:21 +0100

    chore: initial commit

--- renderer/usePageContext.ts
+++ renderer/usePageContext.ts
@@ -1,20 +1,21 @@
 // Hook `usePageContext()` to make `pageContext` available from any Vue component.
 // See https://vite-plugin-ssr.com/pageContext-anywhere
 
-import { inject } from 'vue'
-import type { App } from 'vue'
-import { PageContext } from './types'
+import { PageContextBuiltIn } from "vite-plugin-ssr";
+import type { App } from "vue";
+import { inject } from "vue";
+import { PageContext } from "./types";
 
-export { usePageContext }
-export { setPageContext }
+export { usePageContext };
+export { setPageContext };
 
-const key = Symbol()
+const key = Symbol();
 
-function usePageContext() {
-  const pageContext = inject(key)
-  return pageContext
+function usePageContext(): PageContextBuiltIn & PageContext {
+  const pageContext = inject<PageContextBuiltIn & PageContext>(key)!;
+  return pageContext;
 }
 
 function setPageContext(app: App, pageContext: PageContext) {
-  app.provide(key, pageContext)
+  app.provide(key, pageContext);
 }
